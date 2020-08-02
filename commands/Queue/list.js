92f2270232f8c18a044fd10e68252799b1ded9bf
Tyler <hello@tjrgg.xyz>
2020-08-02 15:50:49 -0500

    feat: enhance list command to list members in queues

--- commands/Queue/list.js
+++ commands/Queue/list.js
@@ -5,17 +5,21 @@ const { getQueue, getQueues } = require('../../utils/queue');
 module.exports = class extends Command {
 
 	constructor(...args) {
-		super(...args, { description: 'Joins a queue.', runIn: [ 'text' ] });
+		super(...args, { description: 'Lists queues or members in queue.', runIn: [ 'text' ], usage: '[name:string]', usageDelim: ' ' });
 	}
 
-	async run(msg) {
-		const queues = getQueues(this.client);
+	async run(msg, [ name ]) {
+		if (name) {
+			const queue = getQueue(this.client, name) || undefined;
+			if (!queue) return msg.sendError(msg.language.get('QUEUE_NOT_FOUND', name));
 
+			return msg.sendQueue(queue);
+		}
+
+		const queues = getQueues(this.client);
 		const queuesList = [];
 
-		for (const queue of queues) {
-			queuesList.push(`- ${msg.language.get('QUEUE_LIST_ITEM', getQueue(this.client, queue))}`);
-		}
+		for (const queue of queues) queuesList.push(`- ${msg.language.get('QUEUE_LIST_ITEM', getQueue(this.client, queue))}`);
 
 		const response = new MessageEmbed();
 		response.setTitle(msg.language.get('QUEUE_LIST_TITLE'));
